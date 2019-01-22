9d9b1c8601c859bc817973c6be4a6e802efbd96b
DrewE <andrew.engelstein@gmail.com>
2019-01-22 11:07:28 -0800

    Add Questions class with ::find_by_id(id) and #initialize(options)

--- questions.rb
+++ questions.rb
@@ -12,3 +12,24 @@ class QuestionsDatabase < SQLite3::Database
 
 end
 
+class Questions
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM questions
+      WHERE id = ?
+    SQL
+    data.map { |datum| Questions.new(datum) }
+  end
+
+  attr_accessor :title, :body, :user_id
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @user_id = options['user_id']
+  end
+  
+end
