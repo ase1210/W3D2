f7f3a90a9fb3b49fb531df9347ee84eeb043c074
DrewE <andrew.engelstein@gmail.com>
2019-01-22 12:00:56 -0800

    Add ::find_by_parent_id(parent_id), ::find_by_user_id(user_id), ::find_by_question_id(question_id), #author, #question, #parent_reply, #child_replies in Reply class, Add ::find_by_id(id) in User class

--- questions.rb
+++ questions.rb
@@ -62,6 +62,15 @@ class User
     data.map { |datum| User.new(datum) }
   end
 
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM users
+      WHERE id = ?
+    SQL
+    data.map { |datum| User.new(datum) }
+  end
+
   attr_accessor :fname, :lname
 
   def initialize(options)
@@ -111,6 +120,33 @@ class Reply
     data.map { |datum| Reply.new(datum) }
   end
 
+  def self.find_by_parent_id(parent_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, parent_id)
+      SELECT *
+      FROM replies
+      WHERE parent_id = ?
+    SQL
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def self.find_by_user_id(user_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT *
+      FROM replies
+      WHERE user_id = ?
+    SQL
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def self.find_by_question_id(question_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT *
+      FROM replies
+      WHERE question_id = ?
+    SQL
+    data.map { |datum| Reply.new(datum) }
+  end
+
   attr_accessor :question_id, :user_id, :body, :parent_id
 
   def initialize(options)
@@ -121,6 +157,21 @@ class Reply
     @user_id = options['user_id']
   end
   
+  def author
+    User.find_by_id(user_id)
+  end
+
+  def question
+    Question.find_by_id(question_id)
+  end
+
+  def parent_reply
+    Reply.find_by_id(parent_id)
+  end
+
+  def child_replies
+    Reply.find_by_parent_id(@id)
+  end
 end
 
 class QuestionLike
